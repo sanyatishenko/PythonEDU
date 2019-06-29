87681c18e375c0b4cddc9687ed3682ef402941fe
alex <alex@test.local>
2019-06-30 00:06:18 +0300

    add dict

---
+++ dict.py
@@ -0,0 +1,27 @@
+#################################
+# Словари (структуры)
+#################################
+
+#       |--------Item--------|
+#       |--Key--||---Value---|
+
+tea = {
+        'Name':   'Tieguanyin',
+        'Color':  'oolong',
+        'Price':  10
+}
+
+coffee = dict(kind='arabica',country='Brasil',price=20)
+
+print(tea)                  # {'Name': 'Tieguanyin', 'Color': 'oolong', 'Price': 10}
+print(coffee)               # {'kind': 'arabica', 'country': 'Brasil', 'price': 20}
+print(tea.keys())           # dict_keys(['Name', 'Color', 'Price'])
+print(tea.values())         # dict_values(['Tieguanyin', 'oolong', 10])
+print(tea['Name'])          # Tieguanyin
+
+tea['country'] = 'China'
+print(tea)                  # {'Name': 'Tieguanyin', 'Color': 'oolong', 'Price': 10, 'country': 'China'}
+
+tea['Price'] += 5
+del tea['Color']
+print(tea)                  # {'Name': 'Tieguanyin', 'Price': 15, 'country': 'China'}
