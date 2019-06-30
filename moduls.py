c431ab032f21fbb90120e49bfde84c003f1e96fb
alex <alex@test.local>
2019-06-30 16:30:45 +0300

    add moduls.py module psevdonim

--- moduls.py
+++ moduls.py
@@ -15,3 +15,13 @@ from mymodule import *
 x = 5
 f = factor(x)                           # Обращение к функции модлуя идиет без имени модуля
 print("Factor ",x,"!\t=\t",f)           # Factor  5 !     =        120
+
+# Псевдонимы модулей
+
+import math as m                        # Обращение к функции модлуя идиет через псевдоним
+
+print("Pi = ",m.pi)
+
+from time import time_ns as ns          # Импорт из модуля одной функции и обращение к ней по псевдониму
+
+print("Time in nanoseconds since Epoche: ",ns())    # Time in nanoseconds since Epoche:  1561900996775119600
