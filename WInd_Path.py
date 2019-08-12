c4fa0f6441c0b88f3c4cb0c0505b11a30a383755
alex <alex@test.local>
2019-08-12 20:33:12 +0300

    Add comments and print line limits

--- WInd_Path.py
+++ WInd_Path.py
@@ -1,27 +1,39 @@
 import itertools
 import random
 
+# Число точек, размерность матрицы
+N = 5
+# Число строк выводимых на печать
+pr = 10
 
-N = 3
+
+# Перестановки маршрутов
 path = itertools.permutations(range(1,N+1))
+
+# Генерируем тестовую матрицу стоимостей
 m = []
-
 m = [[0 for i in range(N)] for j in range(N)]
-
 for i in range(N):
     for j in range(N):
         if i == j:
             m[i][j] = 0
         else:
             m[i][j]=random.randint(1,9)
+'''
+m = ([[0,5,7,4,9],
+      [1,0,6,3,8],
+      [4,9,0,2,1],
+      [9,9,9,0,9],
+      [1,5,2,7,0]])            
+'''            
 
-            
+# Печатем матрицу стоимостей            
 print('*****Input Matrix*****')
 for i in m:
     print(i)
 print('**********************')
 
-
+# Функция расчета стоимости пути
 def path_len(m,p):
     prev = None
     l=0
@@ -31,12 +43,15 @@ def path_len(m,p):
         prev = i-1
     return l
 
-
+# Расчитываем стоимость каждого пути
 result=[]
 for p in path:
     l = path_len(m,p)
+    # Результат - список словарей {Стоимость, путь}
     result += [{'Len':l,'Path':p}]
-    
+
+# Сортируем результат по длине пути
 sort_res = sorted(result,key=lambda k:k['Len'])
-for k in sort_res:
+# Печатем результат
+for k in itertools.islice(sort_res,pr):
     print(k)
